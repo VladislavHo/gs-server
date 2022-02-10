5d312cedf92f156452d72d583cf756aa46c0987a
Vladislav <introwlad@gamil.com>
2022-02-11 01:12:06 +0300

    update

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 const router = require("./router/routers");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT || 5000;
 // const CLIENT_URL = 'https://hopeful-pasteur-d710ff.netlify.app/'
-const MONGODB_KEY = process.env.MONGODB_KEY
-const app = express()
+const MONGODB_KEY = process.env.MONGODB_KEY;
+const app = express();
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: "Access-Control-Allow-Origin",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use("/api", cors());
+app.use("/api", cookieParser());
+app.use("/api", router);
+app.use("/auth", router);
+app.options("/api", cors);
 
-app.use(express.json())
-app.use(cors({ origin: [PORT, "*"], credentials: true }));
-app.use('/api', router)
-app.use('/auth', router)
-app.use('/api', cors())
-app.use('/api', cookieParser())
-
-
-const start = async() =>{
+const start = async () => {
   try {
-    await mongoose.connect(MONGODB_KEY)
-    await app.listen(PORT, console.log(`Server started on port ${PORT}`))
+    await mongoose.connect(MONGODB_KEY);
+    await app.listen(PORT, console.log(`Server started on port ${PORT}`));
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
 
-start()
+start();
